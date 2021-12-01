7cc62df0651a63c63e46c1a8ca788dbce09bf629
Yulian Khomechko <khomechkoyulian@gmail.com>
2021-12-01 07:37:51 +0200

    lazy img

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ const spoiler_btn = document.querySelectorAll('.spoiler__btn');
 const progress_bar = document.querySelector('.progress-bar');
 const about_me_articles = document.querySelectorAll('.about-me__article');
 const spoilers = document.querySelectorAll('.spoiler');
+const avatar = document.querySelector('.avatar');
 
 function showSpoilerContent(e) {
 	const btn = e.target.closest('.spoiler__btn');
 	const spoiler_header = btn.parentElement;
 
 	btn.classList.toggle('.spoiler__btn_active');
-	spoiler_header.nextElementSibling.classList.toggle('spoiler__content_active');	
+	spoiler_header.nextElementSibling.classList.toggle('spoiler__content_active');
 }
 
 spoiler_btn.forEach(el => el.addEventListener('click', showSpoilerContent));
@@ -36,3 +37,8 @@ function startAnimation(targetElement, animationName, animationDelay = 0) {
 
 startAnimation(spoilers, 'slideInX', 600);
 startAnimation(about_me_articles, 'slideInArticles', 600);
+
+avatar.addEventListener('load', () => {
+	avatar.classList.remove('lazy');
+	avatar.src = './img/avatar.jpg';
+});
